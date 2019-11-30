8c1aa8c36849b8ff4ddcddc70febe26e665593b0
cbycongbab <jinwoongoknaver@gmail.com>
2019-11-30 13:45:07 +0900

    work hard

--- Diary_Project/src/Main.java
+++ Diary_Project/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("1234");
 	System.out.println("test");
-	System.out.println("test with friends");
+	System.out.println("test with friend");
 	}
 }
