bf779250098e4475246d9a7460a3745c18ca7c11
cbycongbab <jinwoongoknaver@gmail.com>
2019-12-03 15:22:54 +0900

    import java set

--- Diary_Project/src/Main.java
+++ Diary_Project/src/Main.java
@@ -1,4 +1,15 @@
 
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1234");
