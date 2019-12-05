61473edcffbbf257d2741f4df15e4e900595e762
cbycongbab <jinwoongoknaver@gmail.com>
2019-12-05 13:27:04 +0900

    set with friends

--- Diary_Project/src/Main.java
+++ Diary_Project/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		
 		//Frame 설정 및 추가
 		//Frame 생성 제목표시줄에 다이어리 표시하기 
-		JFrame frame = new JFrame("다이어리");
+		JFrame frame = new JFrame("소박한 다이어리");
 		
 		//창 닫으면 프로그램 종료!
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,7 +35,7 @@ public class Main {
 		frame.getContentPane().add(alarmPanel, "South");
 
 		//TextArea에 컨텐츠가 가리키는 문자열 표시 및 행열 설정
-		JTextArea ta = new JTextArea(content, 40,50);
+		JTextArea ta = new JTextArea(content, 40,45);
 
 		//행 넘기기 true
 		ta.setLineWrap(true);
@@ -46,10 +46,10 @@ public class Main {
 		ta.setToolTipText("이곳에 텍스트를 입력해요..!");
 
 		//textArea 테두리 색 검정, 두께 3
-		Border lineBorder = BorderFactory.createLineBorder(Color.black, 3);
+		Border lineBorder = BorderFactory.createLineBorder(Color.pink, 10);
 
 		//텍스트와 textArea 경계 사이에 여백을 두기 위해서 emptyBorder 생성
-		Border emptyBorder = BorderFactory.createEmptyBorder(7,7,7,7);
+		Border emptyBorder = BorderFactory.createEmptyBorder(2,2,7,7);
 
 		//textArea에 검정테두리와 여백으로 구성된 경계선 설정
 		ta.setBorder(BorderFactory.createCompoundBorder(lineBorder, emptyBorder));
@@ -61,5 +61,5 @@ public class Main {
 
 		frame.setVisible(true); //frame 보이게 설정
 	}
-	static String content = "";
+	static String content = "유리야 사랑해 고마"; //처음에는 아무것도 없도록 페이지 초기화  
 }
